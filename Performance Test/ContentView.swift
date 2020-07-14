6084048369a3975cddc895241aef01801c60f470
Djimon Nowak <dj.nowak@gmx.de>
2020-07-14 16:17:46 +0200

    Initial Commit

---
+++ Performance Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Performance Test
+//
+//  Created by Djimon Nowak on 14.07.20.
+//  Copyright © 2020 Djimon Nowak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
